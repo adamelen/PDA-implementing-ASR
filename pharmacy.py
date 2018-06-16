501095584a6175a7d8f1d4fdba928c00f36640e2
Eleni Adamantidou <adamelen24@gmail.com>
2018-06-17 00:06:18 +0300

    Implement service for pharmacies

--- pharmacy.py
+++ pharmacy.py
@@ -1,3 +1,13 @@
+# coding=utf-8
+
+import ConfigParser
+import requests
+import json
+import codecs
+from places import places
+
 def pharmacy(parameters):
-    textToTell = 'not available yet'
+    ''' Sends a request to google API "Places" to find pharmacies in a certain region and returns a text that contains names and addresses of these pharmacies'''
+
+    textToTell = places(parameters, 'pharmacy', 'pharmacies')
     return textToTell
